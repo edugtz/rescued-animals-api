d9090fed75e94221f13db1f5b535a18cffd4b8fd
edugtz <eduardo.gutierrez.2325@gmail.com>
2019-06-30 08:01:02 -0500

    Re-deploying and removing unnecessary dependencies

--- db/vendor/animal-detail-list.js
+++ db/vendor/animal-detail-list.js
@@ -7,7 +7,7 @@ module.exports = [
         location: 'Guadalajara',
         publication_date: createdAt,
         animal_id: 1,
-        picture: 'https://cdn.pixabay.com/photo/2018/05/10/19/51/schafer-dog-3388580_960_720.jpg',
+        picture: 'https://rescued-animals-assets.s3-us-west-1.amazonaws.com/puppy-1207816_960_720.jpg',
         createdAt,
         updatedAt
      },
@@ -16,7 +16,7 @@ module.exports = [
         location: 'Zapopan',
         publication_date: createdAt,
         animal_id: 2,
-        picture: 'https://cdn.pixabay.com/photo/2016/02/18/18/37/puppy-1207816_960_720.jpg',
+        picture: 'https://rescued-animals-assets.s3-us-west-1.amazonaws.com/schafer-dog-3388580_960_720.jpg',
         createdAt,
         updatedAt
      }
--- gcSetup.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile(process.env.GC_KEY_FILE, process.env.GC_CREDENTIALS, (err) => {});
